7627533f2ca5ca96c35f9c86c489160e84d1e0b4
farziengineer <anand.mohit001@gmail.com>
2019-02-03 15:58:06 +0530

    printed 7th or 8th word depending on whether 7 is null

--- transl.sh
+++ transl.sh
@@ -1,4 +1,4 @@
-content=$(curl  --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTMhrome/23.0.1271.64 Safari/537.11" \
+content=$(curl  --silent --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTMhrome/23.0.1271.64 Safari/537.11" \
 --header "Accept: application/json;q=0.9" \
  "http://translate.google.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=auto&tl=ml&hl=emj&q=$1")
 
@@ -6,5 +6,8 @@ content=$(curl  --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebK
 
 translated_word=(${content//,/ })
 
-echo ${translated_word[7]}
-
+if [ "${translated_word[7]}" == "" ]; then
+	echo ${translated_word[8]}
+else
+	echo ${translated_word[7]}
+fi
