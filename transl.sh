a991bf327883240aa46fae6e9e16300044379f29
farziengineer <anand.mohit001@gmail.com>
2019-02-03 14:20:26 +0530

    Added Accept header

--- transl.sh
+++ transl.sh
@@ -1,2 +1,3 @@
-curl curl --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTMhrome/23.0.1271.64 Safari/537.11" \
+curl  --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTMhrome/23.0.1271.64 Safari/537.11" \
+--header "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" \
  "http://translate.google.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=auto&tl=ml&hl=emj&q=girl""http://translate.google.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=auto&tl=ml&hl=emj&q=girl"
