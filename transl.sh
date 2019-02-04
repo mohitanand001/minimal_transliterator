327a3b736e8cc560ad78994136f2b9a1d5a1eb56
farziengineer <anand.mohit001@gmail.com>
2019-02-04 09:12:07 +0530

    Added url encoder to transliterate multiple words

--- transl.sh
+++ transl.sh
@@ -1,13 +1,19 @@
 content=$(curl  --silent --header  "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTMhrome/23.0.1271.64 Safari/537.11" \
 --header "Accept: application/json;q=0.9" \
- "http://translate.google.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=auto&tl=ml&hl=emj&q=$1")
+--data-urlencode "q=$1" \
+ "http://translate.google.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=auto&tl=ml&hl=emj")
+
+
+# translated_word=(${content//,/ })
 
 # echo $content
 
-translated_word=(${content//,/ })
+IFS=','
+translated_word=($content)
+unset IFS
 
 if [ "${translated_word[7]}" == "" ]; then
-	echo ${translated_word[8]}
+	echo "${translated_word[8]}"
 else
-	echo ${translated_word[7]}
+	echo "${translated_word[7]}"
 fi
